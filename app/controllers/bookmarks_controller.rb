ce9cf56f9763d4be1bf32c5e375cd512b9d623ca
Claire Fernandes <claire@hotdoc.com.au>
2022-11-19 12:32:01 +1100

    finished all controllers

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,11 +1,11 @@
 class BookmarksController < ApplicationController
   def new
-    @list = List.find(params[:id])
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
-    @list = List.find(params[:id])
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(strong_params)
     @bookmark.list = @list
     if @bookmark.save
@@ -16,10 +16,9 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @list = List.find(params[:id])
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@list)
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root 'lists#index'
-  get 'lists', to: 'lists#index'
-  get 'lists/new', to: 'lists#new'
-  post 'lists', to: 'lists#create'
-  get 'lists/:id', to: 'lists#show', as: :list
-  get 'lists/:id/bookmarks/new', to: 'bookmarks#new', as: :new_bookmark
-  post 'lists/:id/bookmarks', to: 'bookmarks#create', as: :bookmark_list
-  delete 'lists/:id', to: 'bookmarks#destroy'
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists', to: 'lists#create'
+  # get 'lists/:id', to: 'lists#show', as: :list
+  # get 'lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: :new_bookmark
+  # post 'lists/:list_id/bookmarks', to: 'bookmarks#create', as: :bookmark_list
+  # delete 'bookmarks/:id', to: 'bookmarks#destroy', as: :bookmark
 end
